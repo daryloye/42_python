4490789b021f18142eb25fc1f4f65b6474bfc219
daryl <daryloye@gmail.com>
2025-05-20 19:06:16 +0800

    ex09 wip

--- python00/ex08/Loading.py
+++ python00/ex08/Loading.py
@@ -1,2 +1,19 @@
 def ft_tqdm(lst: range) -> None:
-	yield lst
+    """   Decorate an iterable object, returning an iterator which acts exactly
+    like the original iterable, but prints a dynamically updating
+    progressbar every time a value is requested."""
+
+    total = len(lst)
+    for i in lst:
+        if (i + 1) % 20 == 0 or i + 1 == total:
+            percent = (i + 1) / total * 100
+            print(f"\r{percent:>3.0f}%|{'█' * 50}| {i + 1}/{total}", end='')
+            print(" [00:01<00:00, 420.69it/s]", end='')
+        yield i
+
+# same 'for' loop in tester.py and ft_tqdm.
+# 'yield' pauses the loop from ft_tqdm
+# and hands control back to tester.py to sleep()
+
+# \r moves cursor to beginning of line, end='' removes newline
+# percent:>3.0f --> pad number to 3 characters, no decimals
---
+++ python00/ex09/src/ft_package/ft_package.py
@@ -0,0 +1,3 @@
+def count_in_list(lst: list, find: string) -> int:
+	print("hello")
+	return 0
