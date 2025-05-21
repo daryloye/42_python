324971a7388d8c3f1b3bab957a48ce94d8e9a10e
daryloye <daryloye@gmail.com>
2025-05-22 01:36:45 +0800

    ex05 grey filter need to change

--- module01/ex05/pimp_image.py
+++ module01/ex05/pimp_image.py
@@ -6,38 +6,52 @@ import matplotlib.pyplot as plt
 def ft_invert(array: np.ndarray) -> np.ndarray:
     """Inverts the color of the image received."""
     
-    print("inverting")
-
-    array = 255 - array
-
-    print(array)
-    img = Image.fromarray(array)
-    plt.imsave("invert.png", array)
+    cpy = array.copy()
+    cpy = 255 - cpy
+    plt.imsave("invert.png", cpy)
+    return (cpy)
 
 
 def ft_red(array) -> np.ndarray:
     """Applies red filter to the image received."""
 
-    img = Image.fromarray(array)
-    plt.savefig("red.png")
+    cpy = array.copy()
+    cpy[:, :, 1] = 0      # set G to 0
+    cpy[:, :, 2] = 0      # set B to 0
+    plt.imsave("red.png", cpy)
+    return (cpy)
 
 
-def ft_green(array):
+def ft_green(array) -> np.ndarray:
     """Applies green filter to the image received."""
 
-    img = Image.fromarray(array)
-    plt.savefig("green.png")
+    cpy = array.copy()
+    cpy[:, :, 0] = 0      # set R to 0
+    cpy[:, :, 2] = 0      # set B to 0
+    plt.imsave("green.png", cpy)
+    return (cpy)
 
 
-def ft_blue(array):
+def ft_blue(array) -> np.ndarray:
     """Applies blue filter to the image received."""
 
-    img = Image.fromarray(array)
-    plt.savefig("blue.png")
+    cpy = array.copy()
+    cpy[:, :, 0] = 0      # set R to 0
+    cpy[:, :, 1] = 0      # set G to 0
+    plt.imsave("blue.png", cpy)
+    return (cpy)
 
 
 def ft_grey(array):
     """Applies grey filter to the image received."""
 
-    img = Image.fromarray(array)
-    plt.savefig("grey.png")
+    ## grey : =, /
+    cpy = array.copy()
+    grey = (0.2989 * cpy[:, :, 0] +
+            0.5870 * cpy[:, :, 1] +
+            0.1140 * cpy[:, :, 2]).astype(np.uint8)
+    cpy[:, :, 0] = grey
+    cpy[:, :, 1] = grey
+    cpy[:, :, 2] = grey
+    plt.imsave("grey.png", cpy)
+    return (cpy)
